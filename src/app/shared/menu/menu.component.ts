30ad072a645d1cab26cd9d01a28a386babe0c679
Charmy Shah <charmyshah@Charmys-MacBook-Pro.local>
2018-01-07 15:20:12 +0530

    Login integration with firebase

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ModalDirective } from 'ngx-bootstrap/modal/modal.component';
+import * as firebase from 'firebase'; 
 
 @Component({
   selector: 'app-menu',
@@ -10,6 +11,31 @@ export class MenuComponent implements OnInit {
 
   @ViewChild('loginModal') loginModal;
   public loginForm: FormGroup;
+
+  validatorOptions = {
+    excluded: ':disabled',
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      username: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter a username'
+          }
+        }
+      },
+      password: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter a password'
+          }
+        }
+      }
+    }
+  };
 
   ngOnInit() {
     this.loginForm = new FormGroup({
@@ -23,6 +49,17 @@ export class MenuComponent implements OnInit {
   }
 
   login() {
-    console.log(this.loginForm);
+    console.log("Errors Required: ", this.loginForm.controls.username.errors.required);
+    console.log("Dirty: ", this.loginForm.controls.username.dirty);
+    if(this.loginForm.status === 'VALID') {
+      let username = this.loginForm.get('username').value;
+      let password = this.loginForm.get('password').value;
+      firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log("Error Code: ", errorCode);
+        console.log("Error Message: ", errorMessage);
+      });
+    }
   }
 }
